00d16b4749b1ef859758127ac69da167f7506bef
Sayam Alvi <sayamalvi07@gmail.com>
2023-07-30 16:52:09 +0530

    Tailwind working fine

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import {ScrollView, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <ScrollView className="bg-slate-700">
+      <Text>Hello world</Text>
+    </ScrollView>
+  );
+};
+
+export default App;
