099c24e6b8750a7d1fe5eec3f90a6f558632de66
Sayam Alvi <sayamalvi07@gmail.com>
2023-07-31 10:32:01 +0530

    Welcome screen done

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,11 +1,40 @@
-import {View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
-
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {useNavigation} from '@react-navigation/native';
 const WelcomeScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 flex justify-evenly bg-white">
+      <View className="space-y-2">
+        <Text
+          style={{fontSize: wp(10)}}
+          className="text-center font-bold text-gray-700">
+          JARVIS
+        </Text>
+        <Text
+          style={{fontSize: wp(4)}}
+          className="text-center font-semibold text-gray-600">
+          Your Personal AI Voice Assistant
+        </Text>
+      </View>
+      <View className="flex-row justify-center">
+        <Image
+          style={{width: wp(75), height: wp(75)}}
+          source={require('../../assets/images/welcome.png')}
+        />
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Home')}
+        className="bg-emerald-600 mx-5 p-4 rounded-2xl">
+        <Text className="text-center font-bold text-white text-2xl">
+          Get Started
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
